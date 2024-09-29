7b63aec56b42e833e26dc02e04b366b619abc25a
tianshaokai <tianshaokai@tal.com>
2024-09-29 10:58:35 +0800

    修改pag

--- app/src/main/java/com/tianshaokai/study/PAGActivity.java
+++ app/src/main/java/com/tianshaokai/study/PAGActivity.java
@@ -6,17 +6,26 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.libpag.PAGFile;
+import org.libpag.PAGImageView;
 import org.libpag.PAGView;
 
 public class PAGActivity extends AppCompatActivity {
+
+    private PAGView pageView;
+    private PAGImageView pageImageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pag1);
 
-        PAGView pageView = findViewById(R.id.pag_view);
+        pageView = findViewById(R.id.pag_view);
         pageView.setComposition(PAGFile.Load(getAssets(), "pag/fankui.pag"));
         pageView.play();
+
+
+        pageImageView = findViewById(R.id.pag_imageview);
+        pageImageView.setComposition(PAGFile.Load(getAssets(), "pag/pag_blink.pag"));
+        pageImageView.play();
     }
 }
